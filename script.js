d3c182c1b07e65f172e360fbd95204c2328e40d9
Varshini3114 <157264175+Varshini3114@users.noreply.github.com>
2024-09-17 12:24:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Thank you for reaching out! I will get back to you soon.');
 });
+var...typed=new Typed(".loop",{
+    strings:["Varshini Rengasamy"],
+    typeSpeed:150
+    backspeed:150
+looped:true
+})
